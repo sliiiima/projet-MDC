b37ac763c671dedc6da9b2a50a88ba757e836480
salma <ssalita70@gmail.com>
2025-04-01 21:05:45 +0000

    modification

---
+++ resources/views/import/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+@section('mini title', 'Import')
+@section('content')
+    <div class="flex justify-end px-2 py-4">
+        <a href="{{ route('medicaments.create') }}">
+            <button
+                class="bg-blue-600 hover:bg-blue-500 rounded-lg px-4 py-2 text-white font-semibold cursor-pointer">ajouter</button>
+        </a>
+    </div>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        odksjpfjspdfoj
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\MedicamentController;
 use App\Http\Controllers\OrdonnanceController;
 use App\Http\Controllers\PatientController;
 use App\Http\Controllers\TypeMedicamentController;
+use App\Http\Controllers\ImportController;
 
 Route::resource('medicaments', MedicamentController::class);
 Route::resource('patients', PatientController::class);
@@ -13,5 +14,7 @@ Route::resource('ordonnances', OrdonnanceController::class);
 Route::resource('types-medicaments', TypeMedicamentController::class);
 Route::resource('fournisseurs', FournisseurController::class);
 Route::resource('details-medicaments', DetailMedicamentController::class);
+Route::resource('import', ImportController::class);
+
 
 Route::get('/',fn()=>view('dashboard'));
